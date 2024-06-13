490080acfde204cbc18a7fbbfd63d65842d5937b
349aditya <141935291+349aditya@users.noreply.github.com>
2024-06-13 13:54:07 +0530

    Add files via upload

---
+++ currency/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { InputBox } from './components'
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl bg-orange-400 ">Currency Converter App</h1>
+    <InputBox/>
+    <InputBox/>
+    </>
+  )
+}
+
+export default App
